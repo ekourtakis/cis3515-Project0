7faa577ab803b682cd179931de649a996492b99a
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-09-06 14:40:29 -0400

    added name, date, etc to top of main file

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
+/* Emmanuel Kourtakis
+ * Assignment 0
+ * CIS 3515
+ * 9/6/24
+ */
+
 fun main() {
     val square : Shape = Square("square")
     val circle : Shape = Circle("circle")
@@ -9,12 +15,12 @@ fun main() {
     val length : Double = readln().toDouble()
     print("enter width: ")
     val width : Double = readln().toDouble()
-//    square.setDimensions(length, width)
+    square.setDimensions(length, width)
 
     println("CIRCLE")
     print("enter radius: ")
     val radius : Double = readln().toDouble()
-//    circle.setDimensions(radius)
+    circle.setDimensions(radius)
 
     println("TRIANGLE")
     print("enter side A: ")
@@ -23,12 +29,12 @@ fun main() {
     val sideB : Double = readln().toDouble()
     print("enter side C: ")
     val sideC : Double = readln().toDouble()
-//    triangle.setDimensions(sideA, sideB, sideC)
+    triangle.setDimensions(sideA, sideB, sideC)
 
     println("EQUILATERAL TRIANGLE")
     print("enter side: ")
     val side : Double = readln().toDouble()
-//    eqTriangle.setDimensions(side)
+    eqTriangle.setDimensions(side)
 
     printInfo(square)
     printInfo(circle)
